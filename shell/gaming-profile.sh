d9444892222b0d2ce8d3bf202cad469a55131ea5
Nils Skowasch <nils_s89@gmx.de>
2024-09-08 21:23:36 +0200

    - add gaming and music profile

---
+++ shell/gaming-profile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python_bin="$(dirname $0)/../venv/bin/python"
+python_file="$(dirname $0)/../g6_cli.py"
+eval "$python_bin $python_file --set-surround Enabled --set-surround-value 25 --set-crystalizer Enabled --set-crystalizer-value 20 --set-bass Enabled --set-bass-value 20 --set-smart-volume Disabled --set-dialog-plus Disabled"
